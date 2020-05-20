a83aa7ba3f79ecb0ea6cb2f54c0b74ea532104bc
Petar Baykov <petbaik26@gmail.com>
2020-05-20 21:59:22 +0300

    Working on Login page component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props)  => {
+
+  console.log('here');
+
+  return (
+    <input />
+  );
+}
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-
-export default class Login extends Component {
-
-  render() {
-    return (
-      <div>Login</div>
-    )
+export default () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
   }
+
+  const onInputChange = () => {
+
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <Input />
+        <Input />
+        <Button>
+          Login
+        </Button>
+      </form>
+    </div>
+  )
 }
